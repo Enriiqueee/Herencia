57d03174a22d91542ae32b2db4972024faaf7f3d
enriiqueee <kikiarenas46@gmail.com>
2023-02-06 11:12:57 +0100

    Creacion de packages and models

---
+++ src/com/iesam/enrique/domain/models/Barbacoa.java
@@ -0,0 +1,13 @@
+package com.iesam.enrique.domain.models;
+
+public class Barbacoa extends Pizza{
+    private boolean picante;
+
+    public boolean isPicante() {
+        return picante;
+    }
+
+    public void setPicante(boolean picante) {
+        this.picante = picante;
+    }
+}
